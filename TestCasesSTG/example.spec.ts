4607542fd3fc1e87e6ea466f620614ee511bef60
Christian Odevilas <christian.odevilas@deliverit.com.au>
2025-07-09 16:28:34 +0800

    playwright update

--- TestCasesSTG/example.spec.ts
+++ TestCasesSTG/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
+  // Expect a title "to contain" a substring.asd
   await expect(page).toHaveTitle(/Playwright/);
 });
 
