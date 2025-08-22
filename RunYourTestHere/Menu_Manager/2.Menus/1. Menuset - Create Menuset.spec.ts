dfb0b4499e24654ab9b477d7a30f4f2f4c1ae915
Vincent Cuenza <vincentcuenza@gmail.com>
2025-08-22 15:46:53 +0800

    update pathing (2)

---
+++ RunYourTestHere/Menu_Manager/2.Menus/1. Menuset - Create Menuset.spec.ts
@@ -0,0 +1,40 @@
+import { Page, test } from '@playwright/test'
+
+// Pages
+import { LoginPage } from '../../../TestCases/Pages/Menu_Manager/1.Items/login';
+import { SearchPLU } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemSearchItem';
+import { ItemGeneral } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemGeneral';
+import { ItemPlatformPricing, ItemApplyAllPricing } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemPricing';
+import { ItemIngredients } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemIngredients';
+import { ItemModifiers } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemModifiers';
+import { ItemAdvancedEditor } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemAdvanced';
+import { DeploymentPage } from '../../../TestCases/Pages/Menu_Manager/1.Items/deploymentPage';
+import { ItemSaveButton } from '../../../TestCases/Pages/Menu_Manager/1.Items/itemSaveButton';
+
+// Utilities
+import { screenshotFunc } from '../../../TestCases/Utilities/screenshot';
+import { getStoreNameByResolution, selectStore } from '../../../TestCases/Utilities/storeSelector';
+import { addRandomLetters } from  '../../../TestCases/Utilities/getAddDeleteChar';
+import { PLU } from '../../../TestCases/Utilities/getPLU'; 
+import { getOperation, addPrice } from '../../../TestCases/Utilities/getOperation'; 
+import { LoggedPage } from '../../../TestCases/Utilities/logger';
+import { stgStudioUrl, stgLoginCredentials, stgDeploymentsUrl } from '../../../TestCases/Utilities/getCredentialsAndUrl';
+
+
+test.setTimeout(600000); // Set timeout to 10 minutes for the entire test suite
+
+test('Single Item - General Info', async ({page}, testInfo) => {
+  const logged = new LoggedPage(page, testInfo.title, testInfo.project.name);
+  const loggedPage = logged.page;
+
+  await loggedPage.goto(stgStudioUrl);
+
+  // Login to STUDIO
+  const loginPage = new LoginPage(loggedPage);
+  await loginPage.login(stgLoginCredentials.email, stgLoginCredentials.password);
+
+  // Select store
+  await selectStore(loggedPage);
+  const storeName = await getStoreNameByResolution(loggedPage);
+
+});
