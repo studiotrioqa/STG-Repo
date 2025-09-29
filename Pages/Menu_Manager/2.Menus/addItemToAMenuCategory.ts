d33a79d26f7fd997cf5d87be66de594e7c308f62
Studio QA <studiotrioqa@gmail.com>
2025-09-29 16:34:43 +0800

    Automate/studio 1621 (#16)
    * STUDIO-1635 >> Initial Commit
    
    * STUDIO-1636 >> Intial Commit
    
    * STUDIO-1621 >> Initial Commit
    
    * STUDIO-1621 >> static way to define group items
    
    ---------
    
    Co-authored-by: Vincent Cuenza <vincentcuenza.work@gmail.com>

--- Pages/Menu_Manager/2.Menus/addItemToAMenuCategory.ts
+++ Pages/Menu_Manager/2.Menus/addItemToAMenuCategory.ts
@@ -19,8 +19,10 @@ export class AddGroupItemToMenuCategory {
         await this.page.locator('div[title].item_card-add__skoxS.card').nth(0).click();
         await this.page.getByRole('searchbox', { name: 'Search PLU / Item Name here' }).fill(GroupPLU);
         await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(0).click();
-        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(1).click();
-        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(2).click();
+        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(6).click();
+        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(12).click();
+        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(18).click();
+        await this.page.locator('[data-test-id="virtuoso-item-list"] div').nth(24).click();
         await this.page.getByRole('checkbox', { name: 'Auto Group' }).check();
     }
 }
